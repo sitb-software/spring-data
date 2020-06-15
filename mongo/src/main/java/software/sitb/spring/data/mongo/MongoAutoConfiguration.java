2dda9b8909182f42bf89a284a1d48a98ae30b33b
sean <sean.snow@live.com>
2020-06-15 23:13:41 +0800

    代码优化删除    @Autowired

--- mongo/src/main/java/software/sitb/spring/data/mongo/MongoAutoConfiguration.java
+++ mongo/src/main/java/software/sitb/spring/data/mongo/MongoAutoConfiguration.java
@@ -35,7 +35,6 @@ public class MongoAutoConfiguration {
     }
 
     @Bean
-    @Autowired
     @ConditionalOnMissingBean
     public IdRepository mongodbIdRepository() {
         return new MongodbIdRepository();
